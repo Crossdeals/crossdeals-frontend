6bf05a42e664bc63c436afa53ba9bc86e4f79837
fortyfiveacp <nng32@my.bcit.ca>
2025-12-01 18:19:02 -0800

    Add unit tests

--- test/test-util.js
+++ test/test-util.js
@@ -77,15 +77,88 @@ describe('Util: handleResponse', () => {
   });
 })
 
-describe('Util: gameListToHomeScreen', () => {
-  let apiGameList = homeScreenData;
-  it('Should convert properly using wishlist data from response', () => {
-    chai.expect(gameListToHomeScreen(
-      "Custom Header",
-      apiGameList,
-      false
+describe('Util: gameDetailsToDetailsScreen', () => {
+  it('Should convert properly with no wishlist data', () => {
+    chai.expect(gameDetailsToDetailsScreen(
+      dummyGameData
     )).to.eql({
-
+      "id": "692543fad58b61f4326764e3",
+      "title": "Minecraft",
+      "publisher": "Mojang Studios",
+      "year": 2011,
+      "platforms": [
+        "PS5",
+        "PC",
+        "Xbox Series X|S"
+      ],
+      "description": "Minecraft is a sandbox game developed and published by Mojang Studios. Formally released on 18 November 2011 for personal computers following its initial public alpha release on 17 May 2009, it has been ported to numerous platforms, including mobile devices and various video game consoles.",
+      "isWishlisted": undefined
     })
+  });
+
+  it('Should convert properly with wishlist data', () => {
+    chai.expect(gameDetailsToDetailsScreen(
+      dummyGameDataWishlisted
+    )).to.eql({
+      "id": "692543fad58b61f4326764e3",
+      "title": "Minecraft",
+      "publisher": "Mojang Studios",
+      "year": 2011,
+      "platforms": [
+        "PS5",
+        "PC",
+        "Xbox Series X|S"
+      ],
+      "description": "Minecraft is a sandbox game developed and published by Mojang Studios. Formally released on 18 November 2011 for personal computers following its initial public alpha release on 17 May 2009, it has been ported to numerous platforms, including mobile devices and various video game consoles.",
+      "isWishlisted": true
+    })
+  });
+})
+
+describe('Util: gamePricingToDetailsScreen', () => {
+  it('Should convert properly', () => {
+    chai.expect(gamePricingToDetailsScreen(
+      dummyGameData
+    )).to.eql([
+      {
+        "gameId": "692543fad58b61f4326764e3",
+        "storefrontId": "storefront_sonyplaystationstore",
+        "platform": "PS5",
+        "price": 88.67,
+        "originalPrice": 88.91,
+        "lowestPrice": 88.67,
+        "dealEnding": "12/26",
+        "storefrontName": "Sony PlayStation Store",
+        "storefrontLink": "https://store.playstation.com/en-ca/"
+      },
+      {
+        "gameId": "692543fad58b61f4326764e3",
+        "storefrontId": "storefront_epicgamesstore",
+        "platform": "PC",
+        "price": 11.97,
+        "originalPrice": 81.91,
+        "lowestPrice": 11.97,
+        "dealEnding": "12/02",
+        "storefrontName": "Epic Games Store",
+        "storefrontLink": "https://store.epicgames.com/en-US/p/"
+      },
+      {
+        "gameId": "692543fad58b61f4326764e3",
+        "storefrontId": "storefront_xboxstore",
+        "platform": "Xbox Series X|S",
+        "price": 26.86,
+        "originalPrice": 44.89,
+        "lowestPrice": 26.86,
+        "dealEnding": "12/25",
+        "storefrontName": "Xbox Store",
+        "storefrontLink": "https://www.xbox.com/en-CA/"
+      }
+    ])
+  });
+
+  it('Should return empty with no storefronts', () => {
+    chai.expect(gamePricingToDetailsScreen(
+      dummyGameDataNoStorefronts
+    )).to.eql([])
   });
 })
